8827ba5af5b56aa0221f37ecd97f5f2e36097f0c
shixiuhai <15256728901@163.com>
2023-03-17 22:55:01 +0800

    更新了数据库文件

--- 商城/1688跨境站点/goods.sql
+++ 商城/1688跨境站点/goods.sql
@@ -11,17 +11,17 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 17/03/2023 22:47:54
+ Date: 17/03/2023 22:53:15
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for commodity
+-- Table structure for 1688_kj_commodity
 -- ----------------------------
-DROP TABLE IF EXISTS `commodity`;
-CREATE TABLE `commodity`  (
+DROP TABLE IF EXISTS `1688_kj_commodity`;
+CREATE TABLE `1688_kj_commodity`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
   `cate_level` int(0) NULL DEFAULT NULL COMMENT '1 一级标题，2 二级标题，3 三级标题，4 四级标题，5 五级标题',
   `cate_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '标题名称',
@@ -34,17 +34,17 @@ CREATE TABLE `commodity`  (
   `offer_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '商品详情页url',
   `created_time` datetime(0) NULL DEFAULT NULL COMMENT '数据创建时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of commodity
+-- Records of 1688_kj_commodity
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for commodity_detail
+-- Table structure for 1688_kj_commodity_detail
 -- ----------------------------
-DROP TABLE IF EXISTS `commodity_detail`;
-CREATE TABLE `commodity_detail`  (
+DROP TABLE IF EXISTS `1688_kj_commodity_detail`;
+CREATE TABLE `1688_kj_commodity_detail`  (
   `id` bigint(0) NOT NULL,
   `commodity_id` bigint(0) NULL DEFAULT NULL COMMENT '商品表id',
   `user_evaluate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '买家评价',
@@ -53,14 +53,14 @@ CREATE TABLE `commodity_detail`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of commodity_detail
+-- Records of 1688_kj_commodity_detail
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for commodity_trend
+-- Table structure for 1688_kj_commodity_trend
 -- ----------------------------
-DROP TABLE IF EXISTS `commodity_trend`;
-CREATE TABLE `commodity_trend`  (
+DROP TABLE IF EXISTS `1688_kj_commodity_trend`;
+CREATE TABLE `1688_kj_commodity_trend`  (
   `id` int(0) NOT NULL,
   `commodity_id` bigint(0) NULL DEFAULT NULL COMMENT '商品表id',
   `xxx1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE `commodity_trend`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of commodity_trend
+-- Records of 1688_kj_commodity_trend
 -- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
