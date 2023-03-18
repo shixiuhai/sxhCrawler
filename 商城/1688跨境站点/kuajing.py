4bde5de8adeec8ecddd86651ef0069612c51ede9
shixiuhai <15256728901@163.com>
2023-03-19 01:03:55 +0800

    提交了数据最新代码

--- 商城/1688跨境站点/kuajing.py
+++ 商城/1688跨境站点/kuajing.py
@@ -3,7 +3,7 @@ import logging
 import time
 import json
 import pymysql
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.INFO,filename="./kuajing.txt",
                     format='%(asctime)s-%(levelname)s-%(message)s')
 class KJ:
     def __init__(self,host="127.0.0.1",port="3306",user="root",passwd="sxh.200008",database="goods") -> None:
@@ -93,8 +93,8 @@ class KJ:
         except Exception as error:
             logging.error("获取该页商品数据出现错误，错误是%s"%error)
             return {}
-    # 入数据库
-    def insertMysql(self,sql:str)->None:
+    # sql 的增 删 改 
+    def exec_mysql(self,sql:str)->None:
         try:
             cursor=self.db.cursor()
             cursor.execute(sql)
@@ -103,13 +103,44 @@ class KJ:
             cursor.close()
         # pymysql.err.OperationalError: (2003, "Can't connect to MySQL server on '183.134.60.170' (timed out)")
         except Exception as error:
-            logging.error("插入数据库出错，错误是%s"%error)
+            logging.error("执行语句出错，错误是%s"%error)
             # print(error.__str__())
             if "Can't connect to MySQL" in error.__str__():
                 self.db=pymysql.connect(host=self.host,port=int(self.port),user=self.user,passwd=self.passwd,db=self.db)
     
+    # sql 插入 返回 插入id
+    def exec_mysql_id(self,sql:str)->int:
+        try:
+            cursor=self.db.cursor()
+            cursor.execute(sql)
+            insertId=self.db.insert_id()
+            # 插入执行必须要提交事务
+            self.db.commit()
+            cursor.close()
+            return insertId
+        # pymysql.err.OperationalError: (2003, "Can't connect to MySQL server on '183.134.60.170' (timed out)")
+        except Exception as error:
+            logging.error("执行语句出错，错误是%s"%error)
+            # print(error.__str__())
+            if "Can't connect to MySQL" in error.__str__():
+                self.db=pymysql.connect(host=self.host,port=int(self.port),user=self.user,passwd=self.passwd,db=self.db)
+            return 0
+    
+    # sql的查询
+    def select_mysql(self,sql:str)->list:
+        try:
+            cursor=self.db.cursor()
+            cursor.execute(sql)
+            return list(cursor.fetchall())
+        except Exception as error:
+            logging.error("查询语句出错，错误是%s"%error)
+            # print(error.__str__())
+            if "Can't connect to MySQL" in error.__str__():
+                self.db=pymysql.connect(host=self.host,port=int(self.port),user=self.user,passwd=self.passwd,db=self.db)
+            return []
+        
     # 保存标题到数据库
-    def save_cate(self):
+    def save_cate(self,taskId):
         fistCateInformation=self.get_goods_cate(parentId=0,cateLevel=1)
         if fistCateInformation!={}:
             for fistCateItem in fistCateInformation["cate"]["result"]:
@@ -119,9 +150,9 @@ class KJ:
                 # sql插入一级标题
                 createdTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 time.sleep(0.2)
-                self.insertMysql("insert into 1688_kj_commodity_index \
-                                 (parent_id,cate_id,cate_name,created_time,cate_level)\
-                                 values ('%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,1))
+                self.exec_mysql("insert into 1688_kj_commodity_index \
+                                 (parent_id,cate_id,cate_name,created_time,cate_level,task_id)\
+                                 values ('%s','%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,1,taskId))
                 
                 secondCateInformation=self.get_goods_cate(parentId=cateId,cateLevel=2)
                 if secondCateInformation!={}:
@@ -132,9 +163,9 @@ class KJ:
                         # print(parentId,cateId,cateName)
                         # sql插入二级标题
                         time.sleep(0.2)
-                        self.insertMysql("insert into 1688_kj_commodity_index \
-                                 (parent_id,cate_id,cate_name,created_time,cate_level)\
-                                 values ('%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,2))
+                        self.exec_mysql("insert into 1688_kj_commodity_index \
+                                 (parent_id,cate_id,cate_name,created_time,cate_level,task_id)\
+                                 values ('%s','%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,2,taskId))
                 
                         threeCateInformation=self.get_goods_cate(parentId=cateId,cateLevel=3)
                         if threeCateInformation!={}:
@@ -145,9 +176,9 @@ class KJ:
                                 # print(parentId,cateId,cateName)
                                 # sql插入三级标题
                                 time.sleep(0.2)
-                                self.insertMysql("insert into 1688_kj_commodity_index \
-                                                (parent_id,cate_id,cate_name,created_time,cate_level)\
-                                                values ('%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,3))
+                                self.exec_mysql("insert into 1688_kj_commodity_index \
+                                 (parent_id,cate_id,cate_name,created_time,cate_level,task_id)\
+                                 values ('%s','%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,3,taskId))
                                 
                                 fourCateInformation=self.get_goods_cate(parentId=cateId,cateLevel=4)
                                 if fourCateInformation!={}:
@@ -158,36 +189,174 @@ class KJ:
                                         print(parentId,cateId,cateName)
                                         time.sleep(0.2)
                                         # sql插入四级标题
-                                        self.insertMysql("insert into 1688_kj_commodity_index \
-                                                        (parent_id,cate_id,cate_name,created_time,cate_level)\
-                                                        values ('%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,4))
+                                        self.exec_mysql("insert into 1688_kj_commodity_index \
+                                        (parent_id,cate_id,cate_name,created_time,cate_level,task_id)\
+                                        values ('%s','%s','%s','%s','%s','%s')"%(parentId,cateId,cateName,createdTime,4,taskId))
     
     # 从mysql中查询所有的最后一级标题信息
-    def get_last_good(self):
-        pass
-    
+    def get_last_cate(self)->list:
+        result=self.select_mysql("""SELECT id,cate_name,cate_id,cate_level,task_id \
+                                FROM 1688_kj_commodity_index  WHERE cate_id NOT IN \
+                                ( SELECT parent_id FROM 1688_kj_commodity_index)""")
+        return result
+        
+    # 定义一共创建任务的方法
+    def created_task(self,tasktype:int=2)->int:
+        # 任务创建时间
+        createdTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        if tasktype==1:
+            describe="手动爬取任务："
+            yourInput=input("请输入你对任务的描述")
+            describe=describe+yourInput
+            # 创建任务
+            taskId=self.exec_mysql_id("insert into 1688_kj_commodity_task (created_time,type,task_describe) values ('%s','%s','%s')"%(createdTime,tasktype,describe))
+            if taskId==0:
+                logging.error("爬取任务启动失败")
+            else:
+                self.spider(taskId=taskId)
+        if tasktype==2:
+            describe="自动爬取任务"
+            taskId=self.exec_mysql_id("insert into 1688_kj_commodity_task (created_time,type,task_describe) values ('%s','%s','%s')"%(createdTime,tasktype,describe))
+            if taskId==0:
+                logging.error("爬取任务启动失败")
+            else:
+                self.spider(taskId=taskId)
+                
     # 定义一个保存到商品信息表的方法
-    def save_commodity(self):
-        pass
+    def save_commodity(self,item:dict):
+        # qmarks = ', '.join(['%s'] * len(item))
+        # columns = ', '.join(item.keys())
+        self.exec_mysql("insert into 1688_kj_commodity (`index_id`, `cate_level`, `cate_id`, `cate_name`, `gmt_create`, `offer_picurl`, `price`, `good_subject`, `offer_id`, `offer_url`, `created_time`, `total_page`, `task_id`, `rank`) \
+                        values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"\
+                        %(item["index_id"],item["cate_level"],item["cate_id"],item["cate_name"],item["gmt_create"],item["offer_picurl"],item["price"],item["subject"],item["offer_id"],item["offer_url"],item["created_time"],item["total_page"],item["task_id"],item["rank"]))
     
-    # 定义一共获取商品信息表的方法
+    # 定义一个获取商品信息表的方法
     def get_commodity(self):
         pass
     
-    # 定义一共保存到商品详情表的方便
-    def save_detail(self):
+    # 定义一个保存到商品详情表的方便
+    def save_detail(self,item:dict):
         pass
     
-    # 爬取信息，目前考虑到最多四级目录
-    def spider(self):
-        pass
+    # 定义一个爬取商品信息的方法
+    def spider_goods_list(self,indexId,cateName,cateId,cateLevel,taskId):
+        # 第一类是hot 
+        resp=self.get_goods_list(cateLevel=cateLevel,cateId=cateId,pageNo=1,rankType="hot")
+        if resp!={}:
+            totalPage=int(resp["page"]["total"]/resp["page"]["pageSize"] )
+            for pageNo in range(1,totalPage+1):
+                time.sleep(0.3)
+                resp=self.get_goods_list(cateLevel=cateLevel,cateId=cateId,pageNo=pageNo,rankType="hot")
+                # 从resp里提取商品信息
+                for oneItem in resp["result"]:
+                    try:
+                        gmtCreate=oneItem["gmt_create"]
+                        offerPicurl=oneItem["offerPicUrl"]
+                        price=oneItem["price"]
+                        subject=oneItem["subject"]
+                        offerId=oneItem["offerId"]
+                    except Exception as error:
+                        logging.error("提取数据搓洗错误错误是%s"%error)
+                        continue
+                    offerUrl="https://detail.1688.com/offer/"+str(offerId)+".html"
+                    createdTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    totalPage=totalPage
+                    indexId=indexId
+                    cateName=cateName
+                    cateId=cateId
+                    cateLevel=cateLevel
+                    taskId=taskId
+                    rank=1
+                    item={"index_id":indexId,
+                          "cate_level":cateLevel,
+                          "cate_id":cateId,
+                          "cate_name":cateName,
+                          "gmt_create":gmtCreate,
+                          "offer_picurl":offerPicurl,
+                          "price":price,
+                          "subject":subject,
+                          "offer_id":offerId,
+                          "offer_url":offerUrl,
+                          "created_time":createdTime,
+                          "total_page":totalPage,
+                          "task_id":taskId,
+                          "rank":rank}
+                    # 存库
+                    self.save_commodity(item=item)
         
-                                                                         
+        # 第二类是rising
+        resp=self.get_goods_list(cateLevel=cateLevel,cateId=cateId,pageNo=1,rankType="rising")
+        if resp!={}:
+            totalPage=int(resp["page"]["total"]/resp["page"]["pageSize"] )
+            for pageNo in range(1,totalPage+1):
+                time.sleep(0.3)
+                resp=self.get_goods_list(cateLevel=cateLevel,cateId=cateId,pageNo=pageNo,rankType="rising")
+                # 从resp里提取商品信息
+                for oneItem in resp["result"]:
+                    try:
+                        gmtCreate=oneItem["gmt_create"]
+                        offerPicurl=oneItem["offerPicUrl"]
+                        price=oneItem["price"]
+                        subject=oneItem["subject"]
+                        offerId=oneItem["offerId"]
+                    except Exception as error:
+                        logging.error("提取数据搓洗错误错误是%s"%error)
+                        continue
+                    offerUrl="https://detail.1688.com/offer/"+str(offerId)+".html"
+                    createdTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                    totalPage=totalPage
+                    indexId=indexId
+                    cateName=cateName
+                    cateId=cateId
+                    cateLevel=cateLevel
+                    taskId=taskId
+                    rank=2
+                    item={"index_id":indexId,
+                          "cate_level":cateLevel,
+                          "cate_id":cateId,
+                          "cate_name":cateName,
+                          "gmt_create":gmtCreate,
+                          "offer_picurl":offerPicurl,
+                          "price":price,
+                          "subject":subject,
+                          "offer_id":offerId,
+                          "offer_url":offerUrl,
+                          "created_time":createdTime,
+                          "total_page":totalPage,
+                          "task_id":taskId,
+                          "rank":rank}
+                    # 存库
+                    self.save_commodity(item=item)
+                    
+                    
+                    
                 
+    # 爬取信息，目前考虑到最多四级目录
+    def spider(self,taskId):
+        # 保存所有标题信息到index表
+        self.save_cate(taskId)
+        # 从index获取最后一级标题信息
+        lastCateList=self.get_last_cate()
+        if len(lastCateList)!=0:
+            for lastCateItem in lastCateList:
+                # id,cate_name,cate_id,cate_level,task_id
+                indexId=lastCateItem[0]
+                cateName=lastCateItem[1]
+                cateId=lastCateItem[2]
+                cateLevel=lastCateItem[3]
+                taskId=lastCateItem[4]
+                # 进行爬取任务
+                self.spider_goods_list(indexId,cateName,cateId,cateLevel,taskId)
+                
+                                        
 if __name__ == '__main__':
     obj=KJ()
+    obj.created_task()
+    #print(obj.get_last_cate())
+    # obj.spider_goods_list(5389,'机械门锁',1033168,3,1)
+    # print(obj.created_task())
     # print(obj.get_goods_cate(2,2))
     # obj.spider()
-    obj.save_cate()
+    #obj.save_cate(1)
     # print(obj.getgoodsIndex(4))
     # print(obj.get_goods_list(2,97,3))
--- 商城/1688跨境站点/readme.md
+++ 商城/1688跨境站点/readme.md
@@ -1,187 +1,259 @@
-## 使用sql
+## 1688跨境站点数据爬取
 
-```bash
+### 功能说明
 
-(SELECT
-	
-  id,
-	cate_name,
-	cate_id ,
-	count(cate_id)
-FROM
-	1688_kj_commodity_index 
-WHERE
-	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index )  GROUP BY id) 
-	
-	
-	SELECT
-	
-  id,
-	cate_name,
-	cate_id ,
-	count(cate_id)
-FROM
-	1688_kj_commodity_index 
-WHERE
-	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index )  GROUP BY id
-	
-	
-	
--- 查询源表数据
-SELECT
-	id,
-	cate_name,
-	cate_id,
-	cate_level,
-	parent_id
-FROM
-	1688_kj_commodity_index 
+* 获取所有的标题信息
+* 获取最后一级标题所有商品信息
+* 管理商品信息和所有标题信息的关系
+* 通过商品信息表获取详情信息
 
+### 系统说明
 
+* 采用了requests实现请求模拟
+* 采用了mysql存储数据
 
--- 查询最后一级是二级的数据
-SELECT
-	id,
-	cate_name,
-	cate_id,
-	cate_level,
-	parent_id
-FROM
-	1688_kj_commodity_index 
-WHERE
-	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) 
-	AND cate_level =2
+### 补充说明
 
-	
--- 查询最后一级是三级的数据
-SELECT
-	id,
-	cate_name,
-	cate_id,
-	cate_level,
-	parent_id
-FROM
-	1688_kj_commodity_index 
-WHERE
-	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) 
-	AND cate_level =1
-	
-	
--- 原表和 最后一级是三级的数据连表 查询出二级标题
-SELECT
-	table1.cate_name two_cate_name,
-	table3.cate_name three_cate_name,
-	table1.parent_id  two_parent_id,
--- 	这个cate_id 是最后一级的cate_id
-	table3.cate_id
-FROM
-	( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-	INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level =3 ) table3 ON table3.parent_id = table1.cate_id
-	
+* 查询的sql如下
 
+  ```bash
+  
+  insert into 1688_kj_commodity_task (created_time,type,`describe`) values ('2023-03-18 21:59:02','2','自动爬取')
+  
+  update 1688_kj_commodity_index set task_id=1 where task_id is NULL
+  
+  SELECT
+    id,
+  	cate_name,
+  	cate_id,
+  	cate_level
+  FROM
+  	1688_kj_commodity_index 
+  WHERE
+  	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) 
+  	
+  	SELECT
+  	
+    id,
+  	cate_name,
+  	cate_id ,
+  	count(cate_id)
+  FROM
+  	1688_kj_commodity_index 
+  WHERE
+  	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index )  GROUP BY id
+  	
+  	
+  	
+  -- 查询源表数据
+  SELECT
+  	id,
+  	cate_name,
+  	cate_id,
+  	cate_level,
+  	parent_id
+  FROM
+  	1688_kj_commodity_index 
+  
+  
+  
+  -- 查询最后一级是二级的数据
+  SELECT
+  	id,
+  	cate_name,
+  	cate_id,
+  	cate_level,
+  	parent_id
+  FROM
+  	1688_kj_commodity_index 
+  WHERE
+  	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) 
+  	AND cate_level =2
+  
+  	
+  -- 查询最后一级是三级的数据
+  SELECT
+  	id,
+  	cate_name,
+  	cate_id,
+  	cate_level,
+  	parent_id
+  FROM
+  	1688_kj_commodity_index 
+  WHERE
+  	cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) 
+  	AND cate_level =1
+  	
+  	
+  -- 原表和 最后一级是三级的数据连表 查询出二级标题
+  SELECT
+  	table1.cate_name two_cate_name,
+  	table3.cate_name three_cate_name,
+  	table1.parent_id  two_parent_id,
+  -- 	这个cate_id 是最后一级的cate_id
+  	table3.cate_id
+  FROM
+  	( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  	INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level =3 ) table3 ON table3.parent_id = table1.cate_id
+  	
+  
+  
+  -- 查询出最后一级是3级标题的所有内容
+  -- 原表和 （原表和最后一级是三级的数据连表） 进行一级标题查询
+  -- 查询出最后一级是3级标题的所有内容
+  -- 原表和 （原表和最后一级是三级的数据连表） 进行一级标题查询
+  SELECT
+  	-- 	针对cate_id去重不知道原因为什么
+  	 DISTINCT(b.cate_id),
+  	b.one_cate_name,
+  	b.two_cate_name,
+  	b.three_cate_name 
+  FROM
+  	(
+  	SELECT
+  		table1.cate_name one_cate_name,
+  		table1_3.two_cate_name,
+  		table1_3.three_cate_name,-- 最后一级的cate_id
+  		table1_3.cate_id 
+  	FROM
+  		( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  		INNER JOIN (
+  		SELECT
+  			table1.cate_name two_cate_name,
+  			table3.cate_name three_cate_name,
+  			table1.parent_id two_parent_id,-- 	这个cate_id 是最后一级的cate_id
+  			table3.cate_id 
+  		FROM
+  			( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  			INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 3 ) table3 ON table3.parent_id = table1.cate_id 
+  		) table1_3 ON table1_3.two_parent_id = table1.cate_id 
+  	) b 
+  
+  
+  
+  -- 查询出最后一级id是二级的内容
+  SELECT
+  	table1.cate_name one_cate_name,
+  	table3.cate_name two_cate_name,
+  	'' three_cate_name,-- 	这个cate_id 是最后一级的cate_id
+  	table3.cate_id 
+  FROM
+  	( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  	INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 2 ) table3 ON table3.parent_id = table1.cate_id
+  	
+  	
+  	
+  	
+  
+  
+  
+  -- 基于最后一级是二级标题和最后一级是三级标题的所有数据合并
+  SELECT
+  	table1_2_3.* 
+  FROM
+  	(
+  		SELECT-- 	针对cate_id去重不知道原因为什么
+  		DISTINCT ( b.cate_id ),
+  		b.one_cate_name,
+  		b.two_cate_name,
+  		b.three_cate_name 
+  	FROM
+  		(
+  		SELECT
+  			table1.cate_name one_cate_name,
+  			table1_3.two_cate_name,
+  			table1_3.three_cate_name,-- 最后一级的cate_id
+  			table1_3.cate_id 
+  		FROM
+  			( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  			INNER JOIN (
+  			SELECT
+  				table1.cate_name two_cate_name,
+  				table3.cate_name three_cate_name,
+  				table1.parent_id two_parent_id,-- 	这个cate_id 是最后一级的cate_id
+  				table3.cate_id 
+  			FROM
+  				( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  				INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 3 ) table3 ON table3.parent_id = table1.cate_id 
+  			) table1_3 ON table1_3.two_parent_id = table1.cate_id 
+  		) b 
+  	) table1_2_3 UNION
+  SELECT
+  	table1_2.* 
+  FROM
+  	(
+  		SELECT-- 	这个cate_id 是最后一级的cate_id
+  		table3.cate_id,
+  		table1.cate_name one_cate_name,
+  		table3.cate_name two_cate_name,
+  		'' three_cate_name 
+  	FROM
+  		( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  		INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 2 ) table3 ON table3.parent_id = table1.cate_id 
+  	) table1_2
+  	
+  	
+  	
+  -- 重要	
+  -- 查询出所有商品信息表
+  SELECT
+    tabl1_2_3.one_cate_name,
+  	tabl1_2_3.two_cate_name,
+  	tabl1_2_3.three_cate_name,
+  	table1.*
+  FROM
+  	1688_kj_commodity table1
+  	LEFT JOIN (
+  	SELECT
+  		table1_2_3.* 
+  	FROM
+  		(
+  			SELECT-- 	针对cate_id去重不知道原因为什么
+  			DISTINCT ( b.cate_id ),
+  			b.one_cate_name,
+  			b.two_cate_name,
+  			b.three_cate_name 
+  		FROM
+  			(
+  			SELECT
+  				table1.cate_name one_cate_name,
+  				table1_3.two_cate_name,
+  				table1_3.three_cate_name,-- 最后一级的cate_id
+  				table1_3.cate_id 
+  			FROM
+  				( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  				INNER JOIN (
+  				SELECT
+  					table1.cate_name two_cate_name,
+  					table3.cate_name three_cate_name,
+  					table1.parent_id two_parent_id,-- 	这个cate_id 是最后一级的cate_id
+  					table3.cate_id 
+  				FROM
+  					( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  					INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 3 ) table3 ON table3.parent_id = table1.cate_id 
+  				) table1_3 ON table1_3.two_parent_id = table1.cate_id 
+  			) b 
+  		) table1_2_3 UNION
+  	SELECT
+  		table1_2.* 
+  	FROM
+  		(
+  			SELECT-- 	这个cate_id 是最后一级的cate_id
+  			table3.cate_id,
+  			table1.cate_name one_cate_name,
+  			table3.cate_name two_cate_name,
+  			'' three_cate_name 
+  		FROM
+  			( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
+  			INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 2 ) table3 ON table3.parent_id = table1.cate_id 
+  		) table1_2 
+  	) tabl1_2_3
+  
+  ON table1.cate_id=tabl1_2_3.cate_id;
+  
+  ```
 
--- 查询出最后一级是3级标题的所有内容
--- 原表和 （原表和最后一级是三级的数据连表） 进行一级标题查询
--- 查询出最后一级是3级标题的所有内容
--- 原表和 （原表和最后一级是三级的数据连表） 进行一级标题查询
-SELECT
-	-- 	针对cate_id去重不知道原因为什么
-	 DISTINCT(b.cate_id),
-	b.one_cate_name,
-	b.two_cate_name,
-	b.three_cate_name 
-FROM
-	(
-	SELECT
-		table1.cate_name one_cate_name,
-		table1_3.two_cate_name,
-		table1_3.three_cate_name,-- 最后一级的cate_id
-		table1_3.cate_id 
-	FROM
-		( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-		INNER JOIN (
-		SELECT
-			table1.cate_name two_cate_name,
-			table3.cate_name three_cate_name,
-			table1.parent_id two_parent_id,-- 	这个cate_id 是最后一级的cate_id
-			table3.cate_id 
-		FROM
-			( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-			INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 3 ) table3 ON table3.parent_id = table1.cate_id 
-		) table1_3 ON table1_3.two_parent_id = table1.cate_id 
-	) b 
+  
 
-
-
--- 查询出最后一级id是二级的内容
-SELECT
-	table1.cate_name one_cate_name,
-	table3.cate_name two_cate_name,
-	'' three_cate_name,-- 	这个cate_id 是最后一级的cate_id
-	table3.cate_id 
-FROM
-	( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-	INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 2 ) table3 ON table3.parent_id = table1.cate_id
-	
-	
-	
-	
--- 基于最后一级是二级标题和最后一级是三级标题的所有数据合并
-SELECT
-	table1_2_3.* 
-FROM
-	(
-		SELECT-- 	针对cate_id去重不知道原因为什么
-		DISTINCT ( b.cate_id ),
-		b.one_cate_name,
-		b.two_cate_name,
-		b.three_cate_name
-		
-	FROM
-		(
-		SELECT
-			table1.cate_name one_cate_name,
-			table1_3.two_cate_name,
-			table1_3.three_cate_name,-- 最后一级的cate_id
-			table1_3.cate_id 
-		FROM
-			( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-			INNER JOIN (
-			SELECT
-				table1.cate_name two_cate_name,
-				table3.cate_name three_cate_name,
-				table1.parent_id two_parent_id,-- 	这个cate_id 是最后一级的cate_id
-				table3.cate_id 
-			FROM
-				( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-				INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 3 ) table3 ON table3.parent_id = table1.cate_id 
-			) table1_3 ON table1_3.two_parent_id = table1.cate_id 
-		) b 
-	) table1_2_3 UNION
-SELECT
-	table1_2.* 
-FROM
-	(
-	SELECT
-	-- 	这个cate_id 是最后一级的cate_id
-		table3.cate_id,
-		table1.cate_name one_cate_name,
-		table3.cate_name two_cate_name,
-		'' three_cate_name
-		
-	FROM
-		( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index ) table1
-		INNER JOIN ( SELECT id, cate_name, cate_id, cate_level, parent_id FROM 1688_kj_commodity_index WHERE cate_id NOT IN ( SELECT parent_id FROM 1688_kj_commodity_index ) AND cate_level = 2 ) table3 ON table3.parent_id = table1.cate_id 
-	) table1_2
-	
-	
-	
-	
-	
-	
-	
-
-
-
-```
+* 详情页jsv获取信息逆向待定
 
