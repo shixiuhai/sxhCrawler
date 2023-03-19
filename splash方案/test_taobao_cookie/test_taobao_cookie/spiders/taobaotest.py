8481ce6a6169737f4c70e7969d3801146866c729
shixiuhai <15256728901@163.com>
2023-03-19 12:41:57 +0800

    增加了scrapy测试项目

---
+++ splash方案/test_taobao_cookie/test_taobao_cookie/spiders/taobaotest.py
@@ -0,0 +1,11 @@
+import scrapy
+import logging
+
+class TaobaotestSpider(scrapy.Spider):
+    name = "taobaotest"
+    allowed_domains = ["www.taobao.com"]
+    start_urls = ["http://www.taobao.com/"]
+
+    def parse(self, response):
+        pass
+        logging.info(response)
