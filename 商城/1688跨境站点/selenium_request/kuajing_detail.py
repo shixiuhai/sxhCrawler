ca108afa55734ac2856a2f5e745fe90e7f5a8ee8
shixiuhai <15256728901@163.com>
2023-03-21 01:30:47 +0800

    更新了代码

--- 商城/1688跨境站点/selenium_request/kuajing_detail.py
+++ 商城/1688跨境站点/selenium_request/kuajing_detail.py
@@ -18,11 +18,11 @@ class ParseLink:
                 url:str,
                 cookies:list,
                 executablePath:str,
-                sourceUrl:str="",
+                sourceUrl:str,
                 timeOut:int=3,
                 EnableLinuxRoot=False,
                 Enableincogniton=False,
-                EnableHeadless=True,
+                EnableHeadless=False,
                 EnableCookies=True
                 ) -> None:
         # 创建一共option对象
@@ -47,7 +47,7 @@ class ParseLink:
         # 设置关闭开发者模式防止被检测
         self.option.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
         self.option.add_argument('--disable-blink-features=AutomationControlled')
-        self.browser=webdriver.Chrome(executable_path=self.executablePath,chrome_options=self.option)
+        self.browser=webdriver.Chrome(executable_path=self.executablePath,options=self.option)
         self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
             "source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})""",
         })
@@ -62,8 +62,8 @@ class ParseLink:
         if sourceUrl=="":
             self.browser.get(self.url)
         else:
-            self.sourceUrl=sourceUrl
-            self.browser(self.sourceUrl)
+            self.browser(sourceUrl)
+            print("触发")
         # 允许使用cookies
         if EnableCookies:
             # 添加cookies
@@ -170,7 +170,8 @@ if __name__ == '__main__':
             count=count+1
             obj=ParseLink(url="https://detail.1688.com/offer/699297425649.html",
                         cookies=[{'domain': '.1688.com', 'expiry': 1694873969, 'httpOnly': False, 'name': 'tfstk', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'cefhBgbIi9JQUaNRfMOQiV1KQvJFZSweA_5N_PiAGfv2s15NiIMZ30n4SHEbYY1..'}, {'domain': '.1688.com', 'expiry': 1679926773, 'httpOnly': False, 'name': 'is_identity', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'buyer'}, {'domain': '.1688.com', 'expiry': 1679926773, 'httpOnly': False, 'name': 'aliwwLastRefresh', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': '1679321973471'}, {'domain': '.1688.com', 'expiry': 1694873969, 'httpOnly': False, 'name': 'l', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'fBE3ujjPNNMuDc6bKOfZPurza779SIRAguPzaNbMi9fP9H_B5yoCW1MToUA6CnGVFsi2R3o-P8F6BeYBqffjeFRtuQIXdpMmnmOk-Wf..'}, {'domain': '.1688.com', 'httpOnly': False, 'name': '__mwb_logon_id__', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'tb80111606'}, {'domain': '.1688.com', 'expiry': 1679393966, 'httpOnly': False, 'name': 'last_mid', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'b2b-345151005463a30'}, {'domain': '.1688.com', 'httpOnly': False, 'name': '__cn_logon_id__', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'tb80111606'}, {'domain': '.1688.com', 'expiry': 1679329177, 'httpOnly': False, 'name': '_show_user_unbind_div_', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'b2b-345151005463a30_false'}, {'domain': '.1688.com', 'httpOnly': False, 'name': 'unb', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '3451510054'}, {'domain': '.1688.com', 'expiry': 1679926740, 'httpOnly': False, 'name': '_m_h5_tk', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '05a223edc1c87ddb5042e2c17ecb30dc_1679332020508'}, {'domain': '.1688.com', 'expiry': 1713881966, 'httpOnly': False, 'name': 'ali_apache_track', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'c_mid=b2b-345151005463a30|c_lid=tb80111606|c_ms=1'}, {'domain': '.1688.com', 'httpOnly': True, 'name': 'csg', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '71599057'}, {'domain': '.1688.com', 'httpOnly': True, 'name': 'sgcookie', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'E100xUqn1kbQTwa92pq2qk8%2BkXlFv2mCjeEk2IGGn7T4ffkun16n6jnYr25eGpL0hxYJF3JxiJL%2FXqfeN8%2F%2Fak7LrEhaYPw4WyAopxX%2FjINUcrs%3D'}, {'domain': '.1688.com', 'httpOnly': True, 'name': 'cookie17', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'UNQyQxMqUdx1nQ%3D%3D'}, {'domain': '.1688.com', 'expiry': 1679329177, 'httpOnly': False, 'name': '_show_force_unbind_div_', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'b2b-345151005463a30_false'}, {'domain': '.1688.com', 'httpOnly': True, 'name': 'cookie1', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'U%2BGWz3AsFiX%2BQb4KVw17j51DAUP9jxfiN9Dd%2FomAUJ8%3D'}, {'domain': '.1688.com', 'httpOnly': True, 'name': 'uc4', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'nk4=0%40FY4GsvRHfRNKE%2BdeKAb%2BB7c9OCFj&id4=0%40UgP5GPE5h%2FvopPV87sbnzF8qm8ZD'}, {'domain': '.1688.com', 'expiry': 1679926772, 'httpOnly': False, 'name': 'firstRefresh', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': '1679321972484'}, {'domain': '.1688.com', 'httpOnly': False, 'name': 'mwb', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'ng'}, {'domain': '.1688.com', 'expiry': 1713881983, 'httpOnly': False, 'name': 'alicnweb', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'touch_tb_at%3D1679321950680%7Clastlogonid%3Dtb80111606'}, {'domain': '.1688.com', 'httpOnly': False, 'name': '_csrf_token', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '1679321966667'}, {'domain': '.1688.com', 'expiry': 1679329178, 'httpOnly': False, 'name': '__rn_alert__', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'false'}, {'domain': '.1688.com', 'expiry': 1679329177, 'httpOnly': False, 'name': '_show_sys_unbind_div_', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'b2b-345151005463a30_false'}, {'domain': '.1688.com', 'expiry': 1679329174, 'httpOnly': False, 'name': '_is_show_loginId_change_block_', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'b2b-345151005463a30_false'}, {'domain': '.1688.com', 'expiry': 1713881943, 'httpOnly': False, 'name': 'cna', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'VlmfHHEGT04CASe8CIWdK7cd'}, {'domain': '.1688.com', 'httpOnly': False, 'name': '_tb_token_', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'ffb7596b3816e'}, {'domain': '.1688.com', 'httpOnly': False, 'name': '__cn_logon__', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'true'}, {'domain': '.1688.com', 'expiry': 1710857966, 'httpOnly': False, 'name': 'lid', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'tb80111606'}, {'domain': '.1688.com', 'httpOnly': False, 'name': 'sg', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '641'}, {'domain': '.1688.com', 'expiry': 1679408342, 'httpOnly': False, 'name': 'xlly_s', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '1'}, {'domain': '.1688.com', 'expiry': 1694873972, 'httpOnly': False, 'name': 'isg', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'BIqKdG5R2t1bFVZxL2XWLeoN23Asew7VWe7BXxTDNl1oxyqB_Ate5dC10zMbN4Zt'}, {'domain': '.1688.com', 'httpOnly': False, 'name': 'ali_apache_tracktmp', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': 'c_w_signed=Y'}, {'domain': '.1688.com', 'expiry': 1679926740, 'httpOnly': False, 'name': '_m_h5_tk_enc', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '3bf670c1e6240d9b6784bc494baff747'}, {'domain': '.1688.com', 'httpOnly': False, 'name': 't', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '5030ddc26b152ff4b5b7798614f9f3ff'}, {'domain': '.1688.com', 'expiry': 1679926772, 'httpOnly': False, 'name': 'lastRefresh', 'path': '/', 'sameSite': 'Lax', 'secure': False, 'value': '1679321972484'}, {'domain': '.1688.com', 'httpOnly': False, 'name': '_nk_', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': 'tb80111606'}, {'domain': '.1688.com', 'httpOnly': True, 'name': 'cookie2', 'path': '/', 'sameSite': 'None', 'secure': True, 'value': '1bb8189afb73e70d741d44ae714785e3'}],
-                        executablePath=r"C:\Users\15256\Documents\Redis-x64-5.0.14.1\chromedriver.exe")
+                        executablePath=r"C:\Users\15256\Documents\Redis-x64-5.0.14.1\chromedriver.exe",
+                        sourceUrl="https://www.1688.com/")
             # obj.scrape_page(condition=EC.visibility_of_element_located,locator=(By.XPATH,'//*[@id="hd_0_container_0"]/div[1]/div[2]/div/div[1]/div[3]/div/div[2]/a/div'))
             # 等待买家评价加载完成说明所有的信息都加载完成了
             if obj.scrape_page(condition=EC.visibility_of_element_located,locator=(By.XPATH,'//*[@id="nc_1_n1z"]')):
@@ -192,6 +193,7 @@ if __name__ == '__main__':
             obj.close_browser()
             print("爬取的页面是%s"%count)
         except Exception as error:
+            print(error)
             print(count)
             continue
     # select 是 css节点
