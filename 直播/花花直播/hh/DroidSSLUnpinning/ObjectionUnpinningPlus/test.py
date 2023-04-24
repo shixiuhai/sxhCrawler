60db2b03b74328bba14017b0af5146dcb1e6a388
shixiuhai <15256728901@163.com>
2023-04-24 15:39:17 +0800

    更新了新代码

--- 直播/花花直播/hh/DroidSSLUnpinning/ObjectionUnpinningPlus/test.py
+++ 直播/花花直播/hh/DroidSSLUnpinning/ObjectionUnpinningPlus/test.py
@@ -1,3 +1,10 @@
 # adb查看正在运行的安卓apk包名
+# adb连接夜神模拟器 .\adb.exe connect 127.0.0.1:62001
 # adb shell dumpsys activity | grep "Run"
+d={"a":"b"}
 # frida -U -f com.nacai.bocai -l .\hooks.js 
+a=1
+if a==2 and d["f"]:
+    print("---")
+else:
+    print("+++")
